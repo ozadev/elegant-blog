48c5308a400885057529542d3b8e30545c8cf07f
Oleh Zaichenko <oleh.zaichenko@gypost.com>
2016-11-23 18:56:20 +0200

    Added sidebar, finished post list section

--- public/js/controllers/app.controller.js
+++ public/js/controllers/app.controller.js
@@ -33,7 +33,34 @@ app.controller("mainCtrl", [
                 author: 'John Doe',
                 commentsNum: 10,
                 posterSrc: './content/0/poster.jpg'
+            },
+            {
+                id: 1,
+                title: 'Lorem ipsum dolor sit amet, his affert platonem intellegat at, blandit maluisset constituto his ne.',
+                preview: 'Et nec consetetur efficiantur. No eum eros reque solet, ius ne expetenda repudiare. Ei his sale prompta suscipiantur, dicunt ancillae copiosae ei mel, nec dicam ridens dolores at. Et his vide inciderint, pri cu convenire molestiae. Ex alterum propriae complectitur mei. Ea vero tritani his, eros adhuc omnium ut per.',
+                category: 4,
+                tags: [3, 4],
+                date: 'Sep 21, 2016',
+                author: 'John Doe',
+                commentsNum: 3,
+                posterSrc: './content/1/poster.jpg'
             }
         ]
+
+        $scope.popularPostsList = [
+            {
+                id: 0,
+                title: 'Lorem ipsum dolor sit amet, his affert platonem intellegat at, blandit maluisset constituto his ne.',
+                date: 'Oct 10, 2016',
+                posterSrc: './content/0/poster-mini.jpg'
+            },
+            {
+                id: 1,
+                title: 'Lorem ipsum dolor sit amet, his affert platonem intellegat at, blandit maluisset constituto his ne.',
+                date: 'Sep 21, 2016',
+                posterSrc: './content/1/poster-mini.jpg'
+            }
+        ]
+
 
     }]);
