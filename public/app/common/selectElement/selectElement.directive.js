223829fdf2825c3c9ced39b17a6897d4c62b4c06
Oleh Zaichenko <oleh.zaichenko@gypost.com>
2017-02-02 18:56:47 +0200

    added test v3

--- public/app/common/selectElement/selectElement.directive.js
+++ public/app/common/selectElement/selectElement.directive.js
@@ -5,7 +5,9 @@
   angular.module('blogApp').directive('selectElement', [
     '$document',
     '$window',
-    function ($document, $window) {
+    '$interval',
+    'textAngularManager',
+    function ($document, $window, $interval, textAngularManager) {
       return {
         templateUrl: '/app/common/selectElement/selectElement.html',
         restrict: 'AE',
@@ -19,22 +21,73 @@
           // element.ready(function() {
           // });
 
+          var savedSelection;
+          var editor = textAngularManager.retrieveEditor('editor1').scope;
+          scope.resultText;
+
+          $interval(function() {
+            var elem = angular.element('<div>')[0];
+            elem.innerHTML = editor.html;
+              if(elem.firstElementChild.tagName === 'P') {
+                elem = elem.firstElementChild;
+              }
+            if (elem.querySelectorAll('span.rangySelectionBoundary')) {
+              elem.querySelectorAll('span.rangySelectionBoundary')
+                  .forEach(function(item) {item.remove()});
+            }
+
+            // console.log(editor.html);
+            var customList = elem.querySelectorAll('span.test-content');
+            if (customList) {
+              customList.forEach(function(item) {
+                item.insertAdjacentHTML('afterEnd', '{{' + item.getAttribute('type') + '}}');
+                item.remove();
+              });
+            }
+            scope.resultText = elem.innerHTML.replace(new RegExp('&nbsp;','g'), '');
+
+            // console.log(resultText);
+
+          }, 100);
+
           scope.variables = {
-            "email_from" : "test@upwork.com",
-            "email_to" : "to@upwork.com",
-            "email_subject" : "our meeting next week"
+            email_from: {
+              name: "test@upwork.com",
+              iconSrc: "/assets/img/icon-doc1.jpg"
+            },
+            email_to : {
+              name : "to@upwork.com",
+              iconSrc: "/assets/img/icon-doc2.jpg"
+            },
+            email_subject : {
+              name : "our meeting next week",
+              iconSrc: "/assets/img/icon-doc3.jpg"
+            }
           };
 
-          scope.text = 'textAngular is a super cool fff {{email_from}} lala {{email_subject}} gdfgdfgdfgdf dfgdfgdfg ';
+          scope.isOpened = false;
+
+          scope.getCursorPosition = getCursorPosition;
+          scope.placeElement = placeElement;
+
+          scope.text = 'Lorem ipsum dolor {{email_from}} consectetur {{email_subject}} adipiscing ';
           scope.textModel = transform(scope.text);
-          reTransform(scope.textModel);
+
 
           //<span class="list-item-elem">val</span>
           // scope.textModel = scope.text;
 
-          scope.$watch(function() {return scope.text}, function() {
-            scope.textModel = transform(scope.text);
-          });
+          // scope.$watch(function() {return scope.text}, function() {
+          //   scope.textModel = transform(scope.text);
+          // });
+
+
+          // scope.$watch(function() {
+          //   return window.getSelection().baseOffset;
+          //
+          // }, function(newVal) {
+          //   scope.carret = newVal;
+          // });
 
           // scope.$watch(function() {return scope.textModel}, function(newVal, prevVal) {
             // if (newVal != prevVal) {
@@ -43,14 +96,14 @@
             // }
           // });
 
-          scope.isOpened = false;
 
-          var status = false;
+          scope.saveCursor = function() {
+            savedSelection = $window.rangy.saveSelection();
+          };
 
           scope.select = function (newItem) {
-
-            status = true;
-            scope.text += ' ' + '{{' + newItem + '}}';
+            var customElem = angular.element('<span class="test-content" value="' + scope.variables[newItem].name + '" type="'+ newItem +'"></span>')[0];
+            placeElement(customElem);
             scope.isOpened = false;
           };
 
@@ -66,37 +119,45 @@
           //////
 
           function transform(text) {
-            // console.log(text);
-            // var varsArr = text.split(/{{(.*?)}}/);
             // var varsArr = text.match(/\{([^}]+)\}}/gm); // with {}
             var varsArr = text.match(/([^{{]*?)\w(?=\}})/gmi); // without {}
-            // console.log(varsArr);
-            // console.log(text);
+
             varsArr.forEach(function(item) {
-              if (status) {
-                console.log(element[0].querySelector('span[data-key="' + item + '"]'));
-                var width = $window.getComputedStyle(element[0].querySelector('span[data-key="' + item + '"]')).getPropertyValue("width");
-                console.log(width);
-                text = text.replace('{{' + item + '}}', '<span class="test-content" style="width: ' + width + '" value="' + scope.variables[item] + '"></span>&nbsp')
-              }
-              else {
-                text = text.replace('{{' + item + '}}', '<span class="test-content" value="' + scope.variables[item] + '"></span>&nbsp')
-              }
-
+              var elem = angular.element('<span>');
+              elem.attr('value', scope.variables[item].name);
+              elem.attr('type', item);
+              elem.attr('style', 'background-image: url(' + scope.variables[item].iconSrc + ')');
+              elem.addClass('test-content');
+              // console.log(elem[0].outerHTML);
+              text = text.replace('{{' + item + '}}', elem[0].outerHTML + '&nbsp')
             });
-
-            // console.log(text);
 
             return text;
           }
 
+          // function reTransform(text) {
+          //   var varsArr = text.match(/<span class=(.*?)\/span>/i);
+          //   console.log(varsArr);
+          // }
 
-          function reTransform(text) {
-            // text += 2;
-            var varsArr = text.match(/<span class=(.*?)\/span>/i);
-            console.log(varsArr);
-            // return text;
+          function getCursorPosition(newItem) {
+            var sel, range;
+            sel = window.getSelection();
+            console.log(sel);
+            if (sel.getRangeAt && sel.rangeCount) {
+              scope.rango = sel.getRangeAt(0);
+              scope.rango.insertNode(document.createTextNode('{{lala}}'));
+
+              return sel.getRangeAt(0);
+            }
           }
+
+          function placeElement(customElem) {
+            editor.displayElements.text[0].focus();
+            $window.rangy.restoreSelection(savedSelection);
+            editor.wrapSelection('insertHTML', customElem.outerHTML + '&nbsp', true);
+          }
+
 
         }
       }
