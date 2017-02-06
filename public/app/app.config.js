2074256745c60ab7206a70a3a2acf1c4f50ddaad
Oleh Zaichenko <oleh.zaichenko@gypost.com>
2017-02-06 19:08:51 +0200

    some layout test

--- public/app/app.config.js
+++ public/app/app.config.js
@@ -9,42 +9,6 @@
     //     }
     // ]);
 
-    angular.module('blogApp').config(['$provide',
-        function($provide) {
-            $provide.decorator('taOptions', ['taRegisterTool', '$delegate',
-                function(taRegisterTool, taOptions) {
-                    // $delegate is the taOptions we are decorating
-                    // here we override the default toolbars specified in taOptions.
-                    taOptions.toolbar = [
-                        ['clear', 'h1', 'h2', 'h3'],
-                        ['ul', 'ol'],
-                        ['bold', 'italics'],
-                        ['insertLink', 'insertVideo']
-                    ];
-
-                    taOptions.disableSanitizer = true;
-
-                    // Create our own insertImage button
-                    taRegisterTool('customHTML', {
-                        iconclass: "fa fa-picture-o",
-                        action: function() {
-                            var textAngular = this;
-                            var savedSelection = rangy.saveSelection();
-                            rangy.restoreSelection(savedSelection);
-                            textAngular.$editor().wrapSelection('insertHTML', '<span style="color: red">U</span>', true);
-
-                            return false;
-                        },
-                    });
-
-                    // Now add the button to the default toolbar definition
-                    // Note: It'll be the last button
-                    taOptions.toolbar[3].push('customHTML');
-                    return taOptions;
-                }
-            ]);
-        }
-    ]);
 
 })();
 
--- public/app/app.module.js
+++ public/app/app.module.js
@@ -3,8 +3,7 @@
 
     angular.module('blogApp', [
         'ui.router',
-        'ngResource',
-        'textAngular'
+        'ngResource'
     // 'ngCookies',
     // 'ngAnimate',
     ]);
