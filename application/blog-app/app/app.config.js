b7a9fbff0861f219669cf9d66e35ffbab673eaa0
Oleh Zaichenko <oza.ukr@gmail.com>
2017-08-06 23:36:22 +0300

    some file replacement

---
+++ application/blog-app/app/app.config.js
@@ -0,0 +1,13 @@
+
+// (function () {
+//     'use strict';
+//
+//     angular.module('blogApp').config([
+//         '$rootScope',
+//         function ($rootScope) {
+//
+//         }
+//     ]);
+//
+// })();
+//
---
+++ application/blog-app/app/app.module.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+
+    angular.module('blogApp', [
+        'ui.router'
+    // 'ngCookies',
+    // 'ngAnimate',
+    ]);
+
+})();
